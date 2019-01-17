992a910fc44923da6beb6d3f13030a910476883c
nikitaChihranov <nikita.chihranov@gmail.com>
2019-01-17 18:41:54 +0400

    Added controller on getting and creating recipes. Also created function to upload photo of recipe.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -16,9 +16,47 @@ let upload = multer({
 let controller = {};
 controller.findAll = async(req, res, next) => {
     try{
-
+        let recipes = await Recipe.find({});
+        res.status(200).json(recipes);
     }catch(e){
         next(new ControllerError(e.message, 400))
     }
-}
+};
+controller.findByName = async(req, res, next) => {
+    try{
+        let name = req.body.name;
+        let recipe = await Recipe.findOne({name});
+        res.status(200).json(recipe);
+    }catch(e){
+        next(new ControllerError(e.message, 400))
+    }
+};
+controller.create = async(req, res, next) => {
+    try{
+        let name = req.body.name;
+        let alreadyExists = await Recipe.findOne({name});
+        if(alreadyExists) console.log('Recipe with such name already exists');
+        else{
+            let recipe = await Recipe.create(req.body);
+            res.status(200).json(recipe);
+        }
+    }catch(e){
+        next(new ControllerError(e.message, 400))
+    }
+};
+controller.uploadPhoto = async(req, res, next) => {
+    try{
+        let recipe = await Recipe.findOne({_id: req.params.id});
+        upload(req, res, (err) => {
+            if(err) console.log(err);
+            let photo = req.file.filename;
+            recipe.photo = photo;
+            recipe.save();
+            res.status(200).json(recipe);
+        })
+    }catch(e){
+        next(new ControllerError(e.message, 400))
+    }
+};
+
 module.exports = controller;
--- models/recipe.js
+++ models/recipe.js
@@ -2,12 +2,11 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let RecipeSchema = new Schema({
-    dish: String,
+    name: String,
     ingredients: String,
     photo: String,
     text: String,
     author: String,
-    text: String
     dateOfCreation: new Date()
 });
 
