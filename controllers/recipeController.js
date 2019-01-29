30841eb6b6e0e1e5c6149b12447002c369188491
nikitaChihranov <nikita.chihranov@gmail.com>
2019-01-30 01:12:26 +0400

    Fix bugs. Handling errors in find by title.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -15,46 +15,45 @@ let upload = multer({
     limits: {fileSize: 200000000}
 }).single('photo');
 let controller = {};
-controller.findAll = async(req, res, next) => {
-    try{
+controller.findAll = async (req, res, next) => {
+    try {
         let recipes = [];
         let recipesToAdd = await AllRecipes.find({});
-        for(let recipe of recipesToAdd ){
+        for (let recipe of recipesToAdd) {
             let length = recipe.history.length;
-            let id = recipe.history[length-1];
+            let id = recipe.history[length - 1];
             let recipe1 = await Recipe.findOne({_id: id});
             recipes.push(recipe1);
         }
         res.status(200).json(recipes);
-    }catch(e){
+    } catch (e) {
         next(new ControllerError(e.message, 400))
     }
 };
-controller.viewAllVersions = async(req, res, next) => {
-    try{
+controller.viewAllVersions = async (req, res, next) => {
+    try {
         let recipes = [];
         let ids = [];
         let Allrecipes = await AllRecipes.findOne({history: req.params.id});
-        for(let recipe of Allrecipes.history){
+        for (let recipe of Allrecipes.history) {
             ids.push(recipe);
         }
-        for(let id of ids){
+        for (let id of ids) {
             let recipe = await Recipe.findOne({_id: id});
             recipes.push(recipe);
         }
         res.status(200).json(recipes);
-    }catch(e){
+    } catch (e) {
         next(new ControllerError(e.message, 400))
     }
 };
-controller.findByName = async(req, res, next) => {
-    try{
-        if(req.params == undefined){
+controller.findByName = async (req, res, next) => {
+    try {
+        if (req.params == undefined) {
             res.status(200).json(new Recipe({
                 text: 'err'
             }));
-        }
-        else {
+        } else {
             let name = req.params.name;
             let recipe = await Recipe.findOne({name});
             if (recipe !== null) {
@@ -69,43 +68,122 @@ controller.findByName = async(req, res, next) => {
                 }));
             }
         }
-    }catch(e){
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
-controller.create = async(req, res, next) => {
-    try{
+controller.sortByDateUp = async (req, res, next) => {
+    try {
+        let recipes = [];
+        let recipesToAdd = await AllRecipes.find({});
+        for (let recipe of recipesToAdd) {
+            let length = recipe.history.length;
+            let id = recipe.history[length - 1];
+            let recipe1 = await Recipe.findOne({_id: id});
+            recipes.push(recipe1);
+        }
+        recipes.sort((a, b) => {
+                if (a.dateOfCreation.getTime() > b.dateOfCreation.getTime()) return -1;
+                if (a.dateOfCreation.getTime() < b.dateOfCreation.getTime()) return 1;
+            }
+        )
+        res.status(200).json(recipes);
+    } catch (e) {
+        next(new ControllerError(e.message, 400));
+    }
+}
+controller.sortByDateDown = async (req, res, next) => {
+    try {
+        let recipes = [];
+        let recipesToAdd = await AllRecipes.find({});
+        for (let recipe of recipesToAdd) {
+            let length = recipe.history.length;
+            let id = recipe.history[length - 1];
+            let recipe1 = await Recipe.findOne({_id: id});
+            recipes.push(recipe1);
+        }
+        recipes.sort((a, b) => {
+                if (a.dateOfCreation.getTime() > b.dateOfCreation.getTime()) return 1;
+                if (a.dateOfCreation.getTime() < b.dateOfCreation.getTime()) return -1;
+    }
+        )
+        res.status(200).json(recipes);
+    } catch (e) {
+        next(new ControllerError(e.message, 400));
+    }
+}
+controller.sortByAZ = async (req, res, next) => {
+    try {
+        let recipes = [];
+        let recipesToAdd = await AllRecipes.find({});
+        for (let recipe of recipesToAdd) {
+            let length = recipe.history.length;
+            let id = recipe.history[length - 1];
+            let recipe1 = await Recipe.findOne({_id: id});
+            recipes.push(recipe1);
+        }
+        recipes.sort((a, b) => {
+                if (a.name > b.name) return 1;
+                if (a.name < b.name) return -1;
+            }
+        )
+        res.status(200).json(recipes);
+    } catch (e) {
+        next(new ControllerError(e.message, 400));
+    }
+}
+controller.sortByZA = async (req, res, next) => {
+    try {
+        let recipes = [];
+        let recipesToAdd = await AllRecipes.find({});
+        for (let recipe of recipesToAdd) {
+            let length = recipe.history.length;
+            let id = recipe.history[length - 1];
+            let recipe1 = await Recipe.findOne({_id: id});
+            recipes.push(recipe1);
+        }
+        recipes.sort((a, b) => {
+                if (a.name > b.name) return -1;
+                if (a.name < b.name) return 1;
+            }
+        )
+        res.status(200).json(recipes);
+    } catch (e) {
+        next(new ControllerError(e.message, 400));
+    }
+}
+controller.create = async (req, res, next) => {
+    try {
         let name = req.body.name;
         let alreadyExists = await Recipe.findOne({name});
-        if(alreadyExists) console.log('Recipe with such name already exists');
-        else{
+        if (alreadyExists) console.log('Recipe with such name already exists');
+        else {
             let createdRecipe = await Recipe.create(req.body);
             let recipe = await AllRecipes.create({});
             recipe.history.push(createdRecipe);
             recipe.save();
             res.status(200).json(recipe);
         }
-    }catch(e){
+    } catch (e) {
         next(new ControllerError(e.message, 400))
     }
 };
-controller.uploadPhoto = async(req, res, next) => {
-    try{
+controller.uploadPhoto = async (req, res, next) => {
+    try {
         let recipeAll = await AllRecipes.findOne({_id: req.params.id});
         let recipe = await Recipe.findOne({_id: recipeAll.history[0]});
         upload(req, res, (err) => {
-            if(err) console.log(err);
-            if(req.file) {
+            if (err) console.log(err);
+            if (req.file) {
                 recipe.photo = req.file.filename;
                 recipe.save();
                 recipeAll.save();
                 res.status(200).json(recipe);
-            }
-            else{
+            } else {
                 next();
             }
         })
-    }catch(e){
+    } catch (e) {
         next(new ControllerError(e.message, 400))
     }
 };
@@ -117,7 +195,7 @@ controller.update = async (req, res, next) => {
         allrecipe.history.push(recipe._id);
         allrecipe.save();
         res.status(200).json(recipe);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
@@ -130,37 +208,39 @@ controller.updatePhoto = async (req, res, next) => {
             recipe.save();
             res.status(200).json(recipe);
         });
-    }catch(e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.delete = async (req, res, next) => {
-    try{
+    try {
         let recipeWithPhoto = await Recipe.findOne({name: req.params.name});
         let id = recipeWithPhoto._id;
         fs.unlink('./public/photos/' + recipeWithPhoto.photo, (err) => (err));
         let allrecipe = await AllRecipes.findOne({history: id});
-        for(let item of allrecipe.history){
+        for (let item of allrecipe.history) {
             await Recipe.findOneAndRemove({_id: item});
         }
         await AllRecipes.findOneAndRemove({history: id});
         res.status(200).json(recipeWithPhoto);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
 controller.deleteAll = async (req, res, next) => {
-    try{
+    try {
         let recipesWithPhotos = await Recipe.find({});
-        await AllRecipes.deleteMany({}, (err) => {});
-        let recipes = await Recipe.deleteMany({}, (err) => {});
-        for(let recipe of recipesWithPhotos) {
-            if(recipe.photo!=='') {
+        await AllRecipes.deleteMany({}, (err) => {
+        });
+        let recipes = await Recipe.deleteMany({}, (err) => {
+        });
+        for (let recipe of recipesWithPhotos) {
+            if (recipe.photo !== '') {
                 fs.unlink('./public/photos/' + recipe.photo, (err) => (err));
             }
         }
         res.status(200).json(recipes);
-    }catch (e) {
+    } catch (e) {
         next(new ControllerError(e.message, 400));
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ let RecipeController = require('../controllers/recipeController')
 
 router.get('/', RecipeController.findAll);
 router.get('/:name', RecipeController.findByName);
+router.get('/sort/s1', RecipeController.sortByDateUp);
+router.get('/sort/s2', RecipeController.sortByDateDown);
+router.get('/sort/s3', RecipeController.sortByAZ);
+router.get('/sort/s4', RecipeController.sortByZA);
 router.get('/viewAllVersions/:id', RecipeController.viewAllVersions);
 router.post('/', RecipeController.create);
 router.post('/uploadPhoto/:id', RecipeController.uploadPhoto);
